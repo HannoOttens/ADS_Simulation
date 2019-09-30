5174a02fad76be79dd49e2e99fe4f8df8ad14c47
Hanno <hanno.ottens@hotmail.com>
2019-09-30 09:18:55 +0200

    Documentatie

--- Python data analysis/readin.py
+++ Python data analysis/readin.py
@@ -1,12 +1,12 @@
 from tabulate import tabulate
 
-
 class Table:
     def __init__(self, data):
         self.headers = data[0]
         self.rows = data[1:]
         self.columns = columnize(self.headers, data[1:])
 
+    # Change the type of a column
     def change_type(self, column, _type):
         if column not in self.headers:
             raise ValueError(f"Header does not exist {column}")
@@ -14,27 +14,31 @@ class Table:
         self.rows = list(map(change_type(_type, header_index), self.rows))
         self.columns = columnize(self.headers, self.rows)
 
+    # Get the columns as arrays instead of a map
     def as_arrays(self):
         arr = []
         for header in self.headers:
             arr.append(self.columns[header])
         return arr
 
+    # Sort on a header
     def sort_on(self, header):
         header_index = self.headers.index(header)
         self.rows.sort(key=sort_on_column(header_index))
         self.columns = columnize(self.headers, self.rows)
 
-
+# Sort key for rows
 def sort_on_column(index):
     return lambda v: v[index]
 
+# Change the type of the item at the index
 def change_type(_type, index):
     def f(row):
         row[index] = _type(row[index])
         return row
     return f
 
+# Columnize data rows
 def columnize(headers, data):
     tbl = {}
     for i in range(len(headers)):
@@ -45,14 +49,10 @@ def columnize(headers, data):
     return tbl
 
 # Remove the \n and split
-
-
 def separate(line):
     return line.replace('\n', '').split(';')
 
 # Read in a file as a table
-
-
 def read_in(filepath):
     with open(filepath) as f:
         lines = f.readlines()
@@ -61,13 +61,12 @@ def read_in(filepath):
     return Table(data)
 
 
-print('hi')
+# Example
 t = read_in("C:/Users/hanno/Google Drive/Vakken/Y4S1/ADS/ADS Simulation Assignment/Code/Python data analysis/Data/artificial-input-data-passengers-01.csv")
 t.change_type('Direction', int)
 t.change_type('From', float)
 t.change_type('To', float)
 t.change_type('PassIn', float)
 t.change_type('PassOut', float)
-print(t.columns['PassOut'])
 t.sort_on('Stop')
 print(tabulate(t.rows, headers=t.headers))
