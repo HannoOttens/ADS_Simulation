d9ac05f6ad0c67d46e5b17b131ea0900bbd56ccd
Hanno <hanno.ottens@hotmail.com>
2019-09-30 09:17:12 +0200

    Sorting

--- Python data analysis/readin.py
+++ Python data analysis/readin.py
@@ -1,15 +1,18 @@
 from tabulate import tabulate
 
+
 class Table:
     def __init__(self, data):
         self.headers = data[0]
         self.rows = data[1:]
-        self.columns = columize(self.headers, data[1:])
-    
+        self.columns = columnize(self.headers, data[1:])
+
     def change_type(self, column, _type):
         if column not in self.headers:
             raise ValueError(f"Header does not exist {column}")
-        self.columns[column] = list(map(_type, self.columns[column]))
+        header_index = self.headers.index(column)
+        self.rows = list(map(change_type(_type, header_index), self.rows))
+        self.columns = columnize(self.headers, self.rows)
 
     def as_arrays(self):
         arr = []
@@ -17,7 +20,22 @@ class Table:
             arr.append(self.columns[header])
         return arr
 
-def columize(headers, data):
+    def sort_on(self, header):
+        header_index = self.headers.index(header)
+        self.rows.sort(key=sort_on_column(header_index))
+        self.columns = columnize(self.headers, self.rows)
+
+
+def sort_on_column(index):
+    return lambda v: v[index]
+
+def change_type(_type, index):
+    def f(row):
+        row[index] = _type(row[index])
+        return row
+    return f
+
+def columnize(headers, data):
     tbl = {}
     for i in range(len(headers)):
         l = []
@@ -27,15 +45,21 @@ def columize(headers, data):
     return tbl
 
 # Remove the \n and split
+
+
 def separate(line):
     return line.replace('\n', '').split(';')
+
+# Read in a file as a table
+
 
 def read_in(filepath):
     with open(filepath) as f:
         lines = f.readlines()
     data = list(map(separate, lines))
-    
+
     return Table(data)
+
 
 print('hi')
 t = read_in("C:/Users/hanno/Google Drive/Vakken/Y4S1/ADS/ADS Simulation Assignment/Code/Python data analysis/Data/artificial-input-data-passengers-01.csv")
@@ -44,4 +68,6 @@ t.change_type('From', float)
 t.change_type('To', float)
 t.change_type('PassIn', float)
 t.change_type('PassOut', float)
+print(t.columns['PassOut'])
+t.sort_on('Stop')
 print(tabulate(t.rows, headers=t.headers))
