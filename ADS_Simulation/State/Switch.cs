7aa52c0437203e8dcba51d8829202c3bbfa3c7ca
Luuk Glorie <l.glorie@students.uu.nl>
2019-10-04 09:34:13 +0200

    Changed switch implementation

--- ADS_Simulation/State/Switch.cs
+++ ADS_Simulation/State/Switch.cs
@@ -6,24 +6,55 @@ namespace ADS_Simulation.NS_State
 {
     class Switch
     {
-        /// <summary>
-        /// Straight track from 'int'
-        /// </summary>
-        Tram? inOccupant;
-        /// <summary>
-        /// Straight track to 'out'
-        /// </summary>
-        Tram? outOccupant;
-        /// <summary>
-        /// Crossing track
-        /// </summary>
-        Tram? crossOccupant;
-
-        Queue<Tram> incomingTrams;
+        private (bool cross, bool arrival, bool departure) state;
 
         public Switch()
         {
-            incomingTrams = new Queue<Tram>();
+            state = (false, false, false);
         }
+
+        public void FreeSwitch(SwitchLane lane)
+        {
+            switch (lane)
+            {
+                case SwitchLane.ArrivalLane:
+                    state.arrival = false;
+                    break;
+                case SwitchLane.DepartureLane:
+                    state.departure = false;
+                    break;
+                case SwitchLane.Cross:
+                    state.cross = false;
+                    break;
+            }
+        }
+
+        public bool UseSwitchIfFree(SwitchLane lane)
+        {
+            bool isFree = false;
+            switch (lane)
+            {
+                case SwitchLane.ArrivalLane:
+                    isFree = !state.cross && !state.arrival;
+                    if (isFree) state.arrival = true;
+                    break;
+                case SwitchLane.DepartureLane:
+                    isFree = !state.cross && !state.departure;
+                    if (isFree) state.departure = true;
+                    break;
+                case SwitchLane.Cross:
+                    isFree = !state.cross && !state.arrival && !state.departure;
+                    if (isFree) state.cross = true;
+                    break;
+            }
+            return isFree;
+        }
+    }
+
+    enum SwitchLane
+    {
+        ArrivalLane = 0,
+        DepartureLane = 1,
+        Cross = 2
     }
 }
