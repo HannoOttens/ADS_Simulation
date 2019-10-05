89352d162c10219efcce5207edc060a0c940531c
Hanno <hanno.ottens@hotmail.com>
2019-10-05 14:51:31 +0200

    EmptyStationStatistic

--- ADS_Simulation/Statistics/EmptyStationStatistic.cs
+++ ADS_Simulation/Statistics/EmptyStationStatistic.cs
@@ -7,9 +7,38 @@ namespace ADS_Simulation.Statistics
 {
     class EmptyStationStatistic : Statistic
     {
+        int lastEventTime;
+
+        int totalEmptyTime;
+        int[] longestEmptyStationTime;
+
+        public EmptyStationStatistic(int startTime, int stationCount)
+        {
+            totalEmptyTime = 0;
+            lastEventTime = startTime;
+            longestEmptyStationTime = new int[stationCount];
+        }
+
         public override void measure(State state)
         {
-            throw new NotImplementedException();
+            for (int stationIndex = 0; stationIndex < state.stations.Count; i++)
+            {
+                Station station = state.stations[stationIndex];
+
+                bool hasOccupant = false;
+                hasOccupant |= station.IsFree();
+                if (station is Endstation endstation)
+                    hasOccupant |= endstation.IsFree(Platform.B);
+
+                if (!hasOccupant)
+                {
+                    int emptyTime = state.time - lastEventTime;
+                    totalEmptyTime += emptyTime;
+                    longestEmptyStationTime[stationIndex] += emptyTime;
+                }
+                else
+                    longestEmptyStationTime[stationIndex] = 0;
+            }
         }
     }
 }
