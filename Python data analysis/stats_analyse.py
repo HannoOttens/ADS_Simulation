dc64f34f09819f1c7adb96d009f021b42292f8a4
Luuk Glorie <l.glorie@students.uu.nl>
2019-10-18 10:53:34 +0200

    add table with confidence interval for each colomn

--- Python data analysis/stats_analyse.py
+++ Python data analysis/stats_analyse.py
@@ -3,12 +3,14 @@ import numpy as np
 import scipy.stats as st
 import os
 from pathlib import Path
+import plotly.graph_objects as go
 
 dir_path = Path(os.path.dirname(os.path.realpath(__file__)))
 out_folder = dir_path.parent.joinpath("ADS_Simulation/output")
 exp = ["base", "exp1"]
 int_max_value = pow(2, 31) - 1
-types = [("average_waiting_time", int),
+alpha = 0.05
+headers_type = [("average_waiting_time", int),
          ("longest_waiting_time", int),
          ("longest_queue", int),
          ("most_left_waiting", int),
@@ -24,6 +26,8 @@ types = [("average_waiting_time", int),
          ("total_delay_pr", int),
          ("total_delay_uc", int),
          ("punctuality", float)]
+intervals = [(21600, int_max_value), (25200, 68400), (27000, 34200), 
+             (34200, 57600), (57600, 64800), (64800, int_max_value)]
 
 tables = dict()
 
@@ -32,36 +36,57 @@ def read_tables(folders):
         exp_folder = out_folder.joinpath(folder)
         tables[folder] = dict()
         for file in os.listdir(exp_folder):
-            time_span = (file.split('_')[0], file.split('_')[1])
+            time_span = (int(file.split('_')[0]), int(file.split('_')[1]))
             tables[folder][time_span] = readin.read_in(exp_folder.joinpath(file))
 
 def set_types():
     for exp in tables.values():
         for table in exp.values():
-            for (k,v) in types:
+            for (k,v) in headers_type:
                 table.change_type(k, v)
 
 
 def t_test(sample1, sample2, column_name):
     tests = dict()
     for k, v in tables[sample1].items():
-        a, b = tables[sample2][k].columns[column_name], v.columns[column_name]
-        N, p, df = len(a), 0.95, len(a) - 1
+        a, b = v.columns[column_name], tables[sample2][k].columns[column_name]
+        N, df = len(a), len(a) - 1
         zipped = zip(a, b)
         diff = list(map(lambda t: t[0] - t[1], zipped))
         a_mean, b_mean, diff_mean = np.mean(a), np.mean(b), np.mean(diff)
         a_std, b_std, diff_std = np.std(a), np.std(b), np.std(diff)
-        t_critical = st.t.ppf(p, df)
+        t_critical = st.t.ppf(1 - (alpha / 2), df)
         stat = t_critical * (diff_std / np.sqrt(N))
         interval = (diff_mean - stat, diff_mean + stat)
-        tests[k] = interval
+        tests[k] = relation(interval)
     return tests
+
+def relation(interval):
+    if interval[0] > 0:
+        return "larger"
+    elif interval[1] < 0:
+        return "smaller"
+    else:
+        return "equal"
+
+def confidence_table(sample1, sample2):
+    headers = list(map(lambda t: t[0], headers_type))
+    data = [intervals]
+    for header in headers:
+        test = t_test(sample1, sample2, header)
+        data.append(list(map(lambda t: test[t], intervals)))
+    headers.insert(0, "interval")
+    return go.Table(header=dict(values=headers), cells=dict(values=data))
+
+
 
 def main():
     read_tables(exp)
     set_types()
-    print(t_test("base", "exp1", "total_delay_uc"))
-    print(t_test("base", "exp1", "total_delay_pr"))
+    table = confidence_table("base", "exp1")
+    fig = go.Figure(data=table)
+    fig.update_layout(width=2200, height=600)
+    fig.show()
 
 
 if __name__ == "__main__":
